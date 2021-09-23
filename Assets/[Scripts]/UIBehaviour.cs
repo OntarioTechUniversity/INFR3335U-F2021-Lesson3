8d49018db5d3d24ef4b74afafd35551d0b54594c
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-09-22 21:31:20 -0400

    Added UIBehaviour Script

---
+++ Assets/[Scripts]/UIBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBehaviour : MonoBehaviour
+{
+    public void OnStartButtonPressed()
+    {
+        Debug.Log("Start Button Pressed!");
+    }
+}
